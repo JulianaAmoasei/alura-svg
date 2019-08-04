d2715a4671113cf9026a0aff84873c630939af9c
JulianaAmoasei <late.parrot@gmail.com>
2019-08-04 20:25:18 -0300

    final draft animated, minor bugs

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,54 @@
-// // TweenMax.to("#coqueiro", 4, {rotation:5, ease:Elastic.easeInOut, transformOrigin:"right bottom", repeat:1})
+// const figura = document.querySelectorAll('.figura')
 
-// TweenMax.to("#nuvem-1", 6, {left: "100%",ease: Linear.easeNone}, 0)
-// TweenMax.to("#nuvem-2", 10, {left: "100%",ease: Linear.easeNone}, 0)
-// TweenMax.to("#nuvem-3", 7, {left: "100%",ease: Linear.easeNone}, 0)
-// TweenMax.to("#nuvem-4", 5, {left: "100%",ease: Linear.easeNone}, 0)
-// TweenMax.to("#nuvem-5", 8, {left: "100%",ease: Linear.easeNone}, 0)
+// figura.forEach((figura) => {
+//     figura.addEventListener("mouseover", () => {
+//       fazerFiguraDancar(figura);
+//     });
+// });
 
-var elmnt = document.getElementById("container-footer");
-var y = window.pageYOffset
+// function fazerFiguraDancar(figura){
+//     figura.classList.toggle('dancinha')
+// }
 
-const container = document.querySelector('#container-footer')
+const figuras = document.querySelectorAll(".figura")
+TweenMax.to(figuras, 0.5, {scale: 0.95, repeat: -1, yoyo: true})
 
-console.log(y, container.scrollTop)
+let posicaoScrollAtual = 0;
+let rolagem = false;
+
+const cardEsq = document.querySelector(".card-esq")
+const cardDir = document.querySelector(".card-dir")
+
+function movimentaCards(scroll_pos) {
+    Object.assign(cardEsq.style,{transform:`translate(${calculo(scroll_pos)}%)`});    
+    Object.assign(cardDir.style,{transform:`translate(${-calculo(scroll_pos)}%)`});    
+}
+
+function calculo(eixoY){
+    return 82 * (eixoY / window.innerHeight);
+  }
+
+//   function calculoOpac(eixoY){
+//     console.log((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight));
+//     return (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+//   }
+
+window.addEventListener('scroll', (e) => {
+  posicaoScrollAtual = window.scrollY;
+  if (!rolagem) {
+    window.requestAnimationFrame(() => {
+      movimentaCards(posicaoScrollAtual);
+      rolagem = false;
+    });
+  }
+  rolagem = true;
+});
+
+const efeitoMarola = document.getElementById("feTurb");
+TweenMax.to(efeitoMarola, 20, {
+    attr: {
+        baseFrequency: `${0.04} ${0.1}`
+    },
+    repeat: -1,
+    yoyo: true
+})
