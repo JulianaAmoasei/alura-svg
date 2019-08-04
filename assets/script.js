2fa6de7928d9c5c48397bbc6357fda26ca4cadfa
JulianaAmoasei <late.parrot@gmail.com>
2019-08-04 02:57:01 -0300

    elementos no html, faltam efeitos de scroll

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+// // TweenMax.to("#coqueiro", 4, {rotation:5, ease:Elastic.easeInOut, transformOrigin:"right bottom", repeat:1})
+
+// TweenMax.to("#nuvem-1", 6, {left: "100%",ease: Linear.easeNone}, 0)
+// TweenMax.to("#nuvem-2", 10, {left: "100%",ease: Linear.easeNone}, 0)
+// TweenMax.to("#nuvem-3", 7, {left: "100%",ease: Linear.easeNone}, 0)
+// TweenMax.to("#nuvem-4", 5, {left: "100%",ease: Linear.easeNone}, 0)
+// TweenMax.to("#nuvem-5", 8, {left: "100%",ease: Linear.easeNone}, 0)
+
+var elmnt = document.getElementById("container-footer");
+var y = window.pageYOffset
+
+const container = document.querySelector('#container-footer')
+
+console.log(y, container.scrollTop)
