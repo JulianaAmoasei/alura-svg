fcdb1868719ef8a24d13282ca095222420f09b39
julianaamoasei <late.parrot@gmail.com>
2019-08-18 01:19:10 -0300

    add mediaq para responsividade, ainda com bugs nos cards e aviao

--- assets/Cards.js
+++ assets/Cards.js
@@ -1,9 +1,9 @@
 class Cards {
 
-  // constructor(){
-  //   this.cardEsq = document.querySelector(".card-esq")
-  //   this.cardDir = document.querySelector(".card-dir")
-  // }
+  constructor(){
+    // this.cardEsq = document.querySelector(".card-esq")
+    // this.cardDir = document.querySelector(".card-dir")
+  }
       
   // calculo(eixoY){
   //   return 82 * (eixoY / window.innerHeight);
@@ -11,23 +11,31 @@ class Cards {
 
   scrollCards() {
     let posicaoScrollAtual = 0;
+    let posicaoInicial = 70
     let rolagem = false;
     posicaoScrollAtual = window.scrollY;
     if (!rolagem) {
       window.requestAnimationFrame(() => {
         this.cardEsq = document.querySelector(".card-esq")
         this.cardDir = document.querySelector(".card-dir")
+        this.featCards = document.getElementsByTagName("article")
 
-        Object.assign(this.cardEsq.style,{transform:`translate(${70 * (posicaoScrollAtual / window.innerHeight)}%)`});    
-        Object.assign(this.cardDir.style,{transform:`translate(${-(70 * (posicaoScrollAtual / window.innerHeight))}%)`});    
+        Object.assign(this.cardEsq.style,{transform:`translate(${posicaoInicial * (posicaoScrollAtual / window.innerHeight)}%)`});    
+        Object.assign(this.cardDir.style,{transform:`translate(${-(posicaoInicial * (posicaoScrollAtual / window.innerHeight))}%)`});    
         
         // Object.assign(this.cardEsq.style,{transform:`translate(${this.calculo(posicaoScrollAtual)}%)`});    
         // Object.assign(this.cardDir.style,{transform:`translate(${-this.calculo(posicaoScrollAtual)}%)`});    
 
         rolagem = false;
       });
+      if (window.innerWidth <= 375){
+        for (let feat of this.featCards){
+          feat.classList.remove("card-esq", "card-dir")
+        }
+      }
     }
     rolagem = true;
+
   }
     
 }
