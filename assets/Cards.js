5eea2850e9bb4e54d7930c22bf5dd434d72985b9
julianaamoasei <late.parrot@gmail.com>
2019-09-12 08:25:35 -0300

    alteracoes cards

--- assets/Cards.js
+++ assets/Cards.js
@@ -9,20 +9,20 @@ class Cards {
       
   scrollCards() {
       window.requestAnimationFrame(this.calculoScroll.bind(this));
-      if (window.innerWidth <= 375){
-        for (let feat of this.featCards){
-          feat.classList.remove("card-esq", "card-dir")
-        }
-      }
+      // if (window.innerWidth <= 375){
+      //   for (let feat of this.featCards){
+      //     feat.classList.remove("card-esq", "card-dir")
+      //   }
+      // }
   }
   
   calculoScroll() {
     const pos = document.getElementsByClassName("section-dicas")[0].getBoundingClientRect()['y']
     if (pos >= -30){
-      // Object.assign(this.cardEsq.style,{transform:`translate(${-(pos + 30)/6}%)`});    
-      // Object.assign(this.cardDir.style,{transform:`translate(${ (pos + 30)/6}%)`});
-      Object.assign(this.cardEsq.style,{transform:`rotateY(${-(pos + 30)/2}deg)`});    
-      Object.assign(this.cardDir.style,{transform:`rotateY(${ (pos + 30)/2}deg)`});
+      Object.assign(this.cardEsq.style,{transform:`translate(${-(pos + 30)/6}%)`});    
+      Object.assign(this.cardDir.style,{transform:`translate(${ (pos + 30)/6}%)`});
+      // Object.assign(this.cardEsq.style,{transform:`rotateY(${-(pos + 30)/2}deg)`});    
+      // Object.assign(this.cardDir.style,{transform:`rotateY(${ (pos + 30)/2}deg)`});
     }  
   }   
 }
